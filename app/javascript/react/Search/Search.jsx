f3a6a67c1b11efde8c8f5fff72b3a372311c8054
b6n <bseverac@servebox.com>
2023-03-25 15:04:06 +0100

    Add slider to filter by price

--- app/javascript/react/Search/Search.jsx
+++ app/javascript/react/Search/Search.jsx
@@ -1,35 +1,44 @@
-import { Layout, Skeleton, Space } from 'antd'
-import React, { useEffect, useState } from 'react'
+import { Empty, Layout, Skeleton, Slider, Space } from 'antd'
+import { t } from 'i18next'
+import React, { useEffect, useMemo, useState } from 'react'
 import { get } from '../common/api'
 import WineCard from './WineCard'
-
 
 export default function Search() {
     const [wineBottles, setWineBottles] = useState([])
 
     useEffect(() => {
-        loadWines()
+        loadWines(10, 50)
     }, [])
 
-    const loadWines = async () => {
-        await get('/api/v1/wine_bottles', {}, (data) => {
+    const loadWines = (min, max) => {
+        get('/api/v1/wine_bottles', { min_price: min, max_price: max }, (data) => {
             setWineBottles(data)
         }, (error) => {
             console.log(error)
         })
     }
-    
-    const wineList = wineBottles.map((wineBottle) => (
+
+    const wineList = wineBottles.length != 0 ? wineBottles.map((wineBottle) => (
         <WineCard key={wineBottle.id} wineBottle={wineBottle} />
-    ))
+    )) : <Empty style={{margin: 'auto'}} />
+
+    const onSliderChange = (value) => {
+        loadWines(value[0], value[1])
+    }
 
     return (
-        <Layout>
-            Hello Wine
-            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: "1em"}}>
+        <Layout style={{textAlign: 'center'}}>
+            <h1>{t("Search.title")}</h1>
+            <Slider style={{marginBottom: "3em"}}
+                    defaultValue={[10, 50]} 
+                    range min={0} max={200}
+                    onAfterChange={onSliderChange}
+                    tooltip={{ open: true, placement: 'bottom', formatter:(value)=> value + '€'}} 
+                    />
+            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: "1em" }}>
                 {wineList}
             </div>
         </Layout>
     )
-} 
- 
+}
